f76fc18d6bdce1aac3981301c5c4f8846ca25a4c
Noemi <noebeatrizm@gmail.com>
2023-03-15 00:19:13 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const tarjeta = document.querySelector('#tarjeta');
+
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
