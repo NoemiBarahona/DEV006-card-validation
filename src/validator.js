54419466e7c0430ddbb9ff5b0b4173848efc724a
Noemi <noebeatrizm@gmail.com>
2023-03-16 14:27:49 -0300

    boton

--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,9 @@ const validator = {
 };
 
 export default validator;
+
+const tarjeta = document.querySelector('#tarjeta');
+
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
