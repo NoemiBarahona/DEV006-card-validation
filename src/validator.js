129e3aee333da819be5d0eda32ef08d982ee991c
Noemi <noebeatrizm@gmail.com>
2023-03-17 11:53:38 -0300

    5

--- src/validator.js
+++ src/validator.js
@@ -8,11 +8,23 @@ const tarjeta = document.querySelector('#tarjeta'),
   botonAbrirFormulario = document.querySelector('#boton-abrir-formulario'),
   formulario = document.querySelector('#formularioTarjeta');
 
+/*--------rotacion de la tarjeta-------*/
 tarjeta.addEventListener('click', () => {
   tarjeta.classList.toggle('active');
 });
 
+/*--------boton de abrir formulario--------*/
 botonAbrirFormulario.addEventListener('click',() => {
   botonAbrirFormulario.classList.toggle('active');
   formulario.classList.toggle('active');
 });
+
+/*--------Select del mes--------*/
+for(let i = 1; i <=12; i++){
+  let opcion = document.createElement('Option');
+  opcion.value = i;
+  opcion.innerText = i;
+  formulario.selectMes.appendChild(opcion);
+} 
+
+/*--------Select del año--------*/
