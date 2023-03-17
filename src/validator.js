5f5447381c21d36a9520c488aaa7efe472828996
Noemi <noebeatrizm@gmail.com>
2023-03-16 21:51:24 -0300

    formulario

--- src/validator.js
+++ src/validator.js
@@ -5,12 +5,14 @@ const validator = {
 export default validator;
 
 const tarjeta = document.querySelector('#tarjeta'),
-      botonAbrirFormulario = document.querySelector('#boton-abrir-formulario');
+  botonAbrirFormulario = document.querySelector('#boton-abrir-formulario'),
+  formulario = document.querySelector('#formularioTarjeta');
 
 tarjeta.addEventListener('click', () => {
-    tarjeta.classList.toggle('active');
+  tarjeta.classList.toggle('active');
 });
 
 botonAbrirFormulario.addEventListener('click',() => {
   botonAbrirFormulario.classList.toggle('active');
+  formulario.classList.toggle('active');
 });
